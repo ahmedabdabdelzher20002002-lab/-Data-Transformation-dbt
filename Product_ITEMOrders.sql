befe8f5149e897b64aa5eb339d648b307dc5dd1d
ahmedabdelzher <ahmedabdabdelzher20002002@gmail.com>
2025-08-08 21:25:16 +0300

    Delete Product_ITEMOrders.sql

--- Product_ITEMOrders.sql
+++
@@ -1,7 +0,0 @@
-select 
-    P.PRODUCT_ID , 
-    P.PRODUCT_NAME ,
-    O.ORDER_ID
-from {{source('stg_production' , 'PRODUCTS')}} P
-inner join {{source('stg_SALES' , 'ORDER_ITEMS')}} O
-on P.PRODUCT_ID = O.PRODUCT_ID
